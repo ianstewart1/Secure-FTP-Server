c8e166eb18b4febe55ffa2d6bfe34a459947cd69
ianstewart1 <46875198+ianstewart1@users.noreply.github.com>
2019-11-27 12:06:44 +0100

    Initializing a few files and testing gitignore

---
+++ src/network.py
@@ -0,0 +1,15 @@
+
+import os
+import sys
+import getopt
+import time
+
+class Network:
+    def __init__(self):
+        pass
+
+    def readMsg(self):
+        pass
+
+    def writeMsg(self):
+        pass
